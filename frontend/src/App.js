2fbfc405ed59732bd61f3161ce684bc66b61e697
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2022-01-30 22:55:29 +0330

    3-3_seprate components product and rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import data from './Data';
+import Product from './components/product'; 
 
 function App() {
   return (
@@ -16,27 +17,8 @@ function App() {
                <div className="row center" >
                  {
                    data.products.map(product =>(
-                    <div key={product._id} className="card">
-                    <a href={`/product/${product._id}`}>
-                        <img className="medium" src={product.image} alt={product.name}></img>
-                    </a>
-                    <div className="card-body">
-                        <a href={`/product/${product._id}`}>
-                            <h2>{product.name}</h2>
-                        </a>
-                        <div className="rating">
-                            <span><i className="fas fa-star" ></i></span>
-                            <span><i className="fas fa-star" ></i></span>
-                            <span><i className="fas fa-star-half-alt" ></i></span>
-                            <span><i className="far fa-star" ></i></span>
-                            <span><i className="far fa-star" ></i></span>
-                        </div>
-                        <div className="price">${product.price}</div>
-                    </div>
-                </div>
-                   ))
-                 }
-                  
+                    <Product key={product._id} product={product} ></Product>
+                   )) }
                </div>
 
             </div>
