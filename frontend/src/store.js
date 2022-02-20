8f0fa5188db2f8371f1056d6303826c793b9dcb4
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2022-02-20 20:38:03 +0330

    9-6_29 last details of payment

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,10 @@ import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 
 import thunk from "redux-thunk";
 import { cartReducer } from "./reducers/cartReducers";
-import { orderCreateReducer } from "./reducers/orderReducers";
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+} from "./reducers/orderReducers";
 import {
   productDetailsReducer,
   productListReducer,
@@ -36,6 +39,7 @@ const reducer = combineReducers({
   userSignin: userSigninReducer,
   userRegister: userRegisterReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
